77cc7a5722c27be0e0592dc380366c0c65a15126
LizethPatino <lizeth0330@outlook.com>
2024-09-20 09:47:30 -0500

    creación de api con parametros y uso de la libreria faker js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 3000;
 
@@ -15,12 +17,44 @@ app.get('/', (req, res) => {
     res.send('Hola este mi server en express :D wiri');
 });
 
-app.get('/nueva-ruta', (req, res) => {
-    res.send('Esto es un nuevo endpoint o ruta :3');
+app.get('/users', (req, res) => {
+    const { limit, offset } = req.query;
+    if ( limit && offset ) {
+        res.json({
+            limit,
+            offset
+        });
+    } else {
+        res.send('No hay parametros :/');
+    }
+   
 });
 
 app.get('/products', (req, res) => {
+    const products= [];
+    const { size } = req.query;
+    const limit = size || 5;
+    for (let index = 0; index < limit; index++) {
+        products.push(
+            {
+                name: faker.commerce.productName(),
+                price: parseInt(faker.commerce.price()),
+                image: faker.image.url(),
+            }
+        )
+        
+    }
     res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    res.json( { id, name: 'Ipad', price: 2500 });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId  } = req.params;
+    res.json( { categoryId, productId });
 });
 
 app.post('/products', (req, res) => {
