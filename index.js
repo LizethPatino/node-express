b59200c77ec2ad0c922c07240f7e968bd193d3f5
LizethPatino <lizeth0330@outlook.com>
2024-09-30 16:44:58 -0500

    whitelist

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+    origin: (origin, callback) => {
+        if(whitelist.includes(origin)){
+            callback(null, true);
+        } else {
+            callback(new Error('no permitido'));
+        }
+    }
+}
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
     res.send('Hola este mi server en express :D wiri');
